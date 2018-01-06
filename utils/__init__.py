5e9cf247e06fb9bc0d1e6bb93cbb987434417aef
Jy-Liu <junyouliu9@gmail.com>
2018-01-06 18:50:02 +0800

    add entity in __init__

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from utils.triangle import Triangle
 from utils.reader import read_tri, xml_read_tri
 from utils.formfactor import FormFactor
+from utils.entity import Entity, Barrel, Cuboid, Globe, Teapot
