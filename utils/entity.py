b06bd999546f91eef44ca7f007eb1a575e831524
Jy-Liu <junyouliu9@gmail.com>
2018-01-06 18:51:14 +0800

    add teapot entity

--- utils/entity.py
+++ utils/entity.py
@@ -2,27 +2,34 @@
 # -*- coding: UTF-8 -*-
 
 import numpy as np
+from utils.triangle import Triangle
 
 
 class Entity(object):
     """docstring for Entity"""
+
     def __init__(self, spec=1.0, refl=0.0, refr=0.0):
         super(Entity, self).__init__()
+
+        self.emission = Triangle.Color()
+        self.reflectivity = Triangle.Color()
+        self.radiosity = Triangle.Color()
+
         self._spec = spec
         self._refl = refl
         self._refr = refr
-        self.emission = 0.0
-        self.reflectivity = 0.0
-        self.radiosity = 0.0
-        self.patch_set = []
+
+        self.transform_matrix = None
+        self.triangle_set = []
         self.name = "entity"
 
-    def create(xml_node):
+    def create(self, xml_node):
         node_name = xml_node.name
 
 
 class Barrel(Entity):
     """docstring for Barrel"""
+
     def __init__(self, spec=1.0, refl=0.0, refr=0.0):
         super(Barrel, self).__init__(spec, refl, refr)
         self.name = "barrel"
@@ -30,6 +37,7 @@ class Barrel(Entity):
 
 class Cuboid(Entity):
     """docstring for Cuboid"""
+
     def __init__(self, spec=1.0, refl=0.1, refr=0.0):
         super(Cuboid, self).__init__(spec, refl, refr)
         self.name = "cuboid"
@@ -37,6 +45,7 @@ class Cuboid(Entity):
 
 class Globe(Entity):
     """docstring for Globe"""
+
     def __init__(self, spec=1.0, refl=0.7, refr=0.0):
         super(Globe, self).__init__(spec, refl, refr)
         self.name = "globe"
@@ -44,7 +53,14 @@ class Globe(Entity):
 
 class Teapot(Entity):
     """docstring for Teapot"""
-    def __init__(self, spec=1.0, refl=0.0, refr=0.8):
-        super(Teapot, self).__init__()
-        self.name = "teapot"
 
+    def __init__(self, spec=1.0, refl=0.0, refr=0.8):
+        super(Teapot, self).__init__(spec, refl, refr)
+        self.name = "teapot"
+        self.teapot_v = self.read()
+        self.teapot_v_count = len(self.teapot_v)
+        self.teapot_t = self.read()
+        self.teapot_t_count = len(self.teapot_t)
+
+    def deserialize():
+        pass
