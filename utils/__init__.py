dfa1559c5a04bc84ce1b62db3da396ccba1a87fd
yistLin <yishen992@gmail.com>
2018-01-09 20:03:41 +0800

    xml writer

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,4 @@ from utils.transform_matrix import TransformMatrix
 from utils.reader import read_tri, xml_read_tri
 from utils.entity import Entity, Barrel, Cuboid, Globe, Teapot
 from utils.formfactor import FormFactor
+from utils.writer import XMLWriter
