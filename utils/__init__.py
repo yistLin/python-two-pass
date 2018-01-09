3871e117b4d2bf01fb212a4d49872df53eb8ea14
Jy-Liu <junyouliu9@gmail.com>
2018-01-09 22:46:07 +0800

    new format for class Triangle

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
 from utils.triangle import Triangle
 from utils.triangle_set import TriangleSet
-from utils.transform_matrix import TransformMatrix
-from utils.reader import read_tri, xml_read_tri
-from utils.entity import Entity, Barrel, Cuboid, Globe, Teapot
-from utils.formfactor import FormFactor
-from utils.writer import XMLWriter
+# from utils.transform_matrix import TransformMatrix
+# from utils.reader import read_tri, xml_read_tri
+# from utils.entity import Entity, Barrel, Cuboid, Globe, Teapot
+# from utils.formfactor import FormFactor
+# from utils.writer import XMLWriter
