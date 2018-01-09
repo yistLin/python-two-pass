8dfcbb5e66980ef80be35705325b0ea5d7d7f095
Jy-Liu <junyouliu9@gmail.com>
2018-01-10 00:20:10 +0800

    new format for class TransformMatrix

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from utils.triangle import Triangle
 from utils.triangle_set import TriangleSet
-# from utils.transform_matrix import TransformMatrix
+from utils.transform_matrix import TransformMatrix
 # from utils.reader import read_tri, xml_read_tri
 # from utils.entity import Entity, Barrel, Cuboid, Globe, Teapot
 # from utils.formfactor import FormFactor
