4619b16b5a273f40977a39c04f3c7369dac29a6d
WindQAQ <b03902042@ntu.edu.tw>
2018-01-10 08:25:34 +0800

    new format for XMLReader

--- utils/reader.py
+++ utils/reader.py
@@ -8,155 +8,129 @@ from copy import deepcopy
 from utils.entity import Entity
 
 
-def xml_read_scene(fname):
-    def read_head(head):
-        scene = {}
-        for obj in head.iter('objectdef'):
-            obj_name = obj.attrib['name']
-            list_of_args = []
-            for entity in obj:
-                entity_name = 'triangleset'
-                attrs = {k: tuple(map(float, v.split(','))) for k, v in entity.items() if k != 'name'}
-                if entity.tag == 'triangleset':
-                    for triangle in entity:
-                        if triangle.tag == 'triangle':
-                            vs = [(p.get('x'), p.get('y'), p.get('z')) for p in triangle.iter('vertex')]
-                        elif triangle.tag == 'trianglenext':
-                            vs = [vs[1]] + [(p.get('x'), p.get('y'), p.get('z')) for p in triangle.iter('vertex')] + [vs[2]]
-                        else:
-                            raise AttributeError("Tag doesn't match either triangle or trianglenext.")
+class XMLReader(object):
+    """docstring for XMLReader"""
 
-                        vertex = {'v{}'.format(v): p for v, p in enumerate(vs)}
-                        list_of_args.append({**vertex, **attrs})
-                else:
-                    entity_name = entity.tag
-                    list_of_args.append(attrs)
+    @staticmethod
+    def read_scene(fname):
+        def read_head(head):
+            scene = {}
+            for obj in head.iter('objectdef'):
+                obj_name = obj.attrib['name']
+                list_of_args = []
+                for entity in obj:
+                    entity_name = 'triangleset'
+                    attrs = {k: list(map(float, v.split(','))) for k, v in entity.items() if k != 'name'}
+                    if entity.tag == 'triangleset':
+                        for triangle in entity:
+                            if triangle.tag == 'triangle':
+                                vs = [[p.get('x'), p.get('y'), p.get('z')] for p in triangle.iter('vertex')]
+                            elif triangle.tag == 'trianglenext':
+                                vs = [vs[1]] + [[p.get('x'), p.get('y'), p.get('z')] for p in triangle.iter('vertex')] + [vs[2]]
+                            else:
+                                raise AttributeError("Tag doesn't match either triangle or trianglenext.")
 
-            if 'mirror' in obj_name:
-                for i in range(len(list_of_args)):
-                    list_of_args[i] = {**list_of_args[i], 'refl': 0.95}
-            scene[obj_name] = Entity.create(entity_name, list_of_args, obj_name)
+                            list_of_args.append({'vertices': vs, **attrs})
+                    else:
+                        entity_name = entity.tag
+                        list_of_args.append(attrs)
+
+                scene[obj_name] = Entity.create(entity_name, list_of_args)
+
+            return scene
+
+        def read_body(body, scene):
+            stack = [(body, -1)]
+            idx = 0
+            trans = {}
+
+            obj_cnt = {obj_name: 0 for obj_name in scene.keys()}
+            obj_trans = {}
+
+            # perform DFS on XML's element tree
+            while stack:
+                node, pidx = stack.pop()
+
+                for elem in reversed(node):
+                    if elem.tag == 'object':
+                        cidx = pidx
+                        obj_name = elem.attrib['name']
+                        suffix = obj_cnt[obj_name]
+                        name = '{}:{}'.format(obj_name, suffix) if suffix else obj_name
+                        if suffix:
+                            scene[name] = deepcopy(scene[obj_name])
+                        obj_cnt[obj_name] += 1
+                        obj_trans[name] = []
+                        while cidx != -1:
+                            t, cidx = trans[cidx]
+                            obj_trans[name].append((t.tag, {k: float(v) for k, v in t.items()}))
+
+                    stack.append((elem, idx))
+                    trans[idx] = (elem, pidx)
+                    idx += 1
+
+            for obj_name in obj_trans.keys():
+                scene[obj_name].transform(obj_trans[obj_name][::-1])
+
+            return scene
+
+        # parse XML
+        tree = ET.parse(fname)
+        root = tree.getroot()
+
+        # get objects definitions & objects translation+rotation info
+        head = root.find('head')
+        body = root.find('body')
+
+        scene = read_head(head)
+        scene = read_body(body, scene)
 
         return scene
 
-    def read_body(body, scene):
-        stack = [(body, -1)]
-        idx = 0
-        trans = {}
+    @staticmethod
+    def read_tri(fname):
+        # important info
+        mat_c = []
+        mat_e = []
+        mat_p = []
+        mat_spec = []
+        mat_refl = []
+        mat_refr = []
 
-        obj_cnt = {obj_name: 0 for obj_name in scene.keys()}
-        obj_trans = {}
+        # parse XML
+        tree = ET.parse(fname)
+        root = tree.getroot()
 
-        # perform DFS on XML's element tree
-        while stack:
-            node, pidx = stack.pop()
+        # get object definitions
+        obj_root = root.find('head').find('objectdef')
 
-            for elem in reversed(node):
-                if elem.tag == 'object':
-                    cidx = pidx
-                    obj_name = elem.attrib['name']
-                    suffix = obj_cnt[obj_name]
-                    name = '{}:{}'.format(obj_name, suffix) if suffix else obj_name
-                    if suffix:
-                        scene[name] = deepcopy(scene[obj_name])
-                    obj_cnt[obj_name] += 1
-                    obj_trans[name] = []
-                    while cidx != -1:
-                        t, cidx = trans[cidx]
-                        obj_trans[name].append((t.tag, {k: float(v) for k, v in t.items()}))
+        # loop through objects
+        for obj in obj_root:
 
-                stack.append((elem, idx))
-                trans[idx] = (elem, pidx)
-                idx += 1
+            # loop through triangles
+            for tri in obj:
+                emission = tri.attrib['emission'].split(',')
+                radiosity = tri.attrib['radiosity'].split(',')
+                spec = tri.attrib['spec']
+                refl = tri.attrib['refl']
+                refr = tri.attrib['refr']
+                mat_c.append([float(r) for r in radiosity])
+                mat_e.append([float(e) for e in emission])
+                mat_spec.append(float(spec))
+                mat_refl.append(float(refl))
+                mat_refr.append(float(refr))
 
-        for obj_name in obj_trans.keys():
-            scene[obj_name].transform(obj_trans[obj_name][::-1])
+                # loop through vertices
+                vertices = []
+                for vtx in tri:
+                    vertices.append([float(vtx.attrib['x']),
+                                     float(vtx.attrib['y']),
+                                     float(vtx.attrib['z'])])
+                mat_p.append(vertices)
 
-        return scene
-
-    # parse XML
-    tree = ET.parse(fname)
-    root = tree.getroot()
-
-    # get objects definitions & objects translation+rotation info
-    head = root.find('head')
-    body = root.find('body')
-
-    scene = read_head(head)
-    scene = read_body(body, scene)
-
-    return scene
-
-
-def xml_read_tri(fname):
-    # important info
-    mat_c = []
-    mat_e = []
-    mat_p = []
-    mat_spec = []
-    mat_refl = []
-    mat_refr = []
-
-    # parse XML
-    tree = ET.parse(fname)
-    root = tree.getroot()
-
-    # get object definitions
-    obj_root = root.find('head').find('objectdef')
-
-    # loop through objects
-    for obj in obj_root:
-
-        # loop through triangles
-        for tri in obj:
-            emission = tri.attrib['emission'].split(',')
-            radiosity = tri.attrib['radiosity'].split(',')
-            spec = tri.attrib['spec']
-            refl = tri.attrib['refl']
-            refr = tri.attrib['refr']
-            mat_c.append([float(r) for r in radiosity])
-            mat_e.append([float(e) for e in emission])
-            mat_spec.append(float(spec))
-            mat_refl.append(float(refl))
-            mat_refr.append(float(refr))
-
-            # loop through vertices
-            vertices = []
-            for vtx in tri:
-                vertices.append([float(vtx.attrib['x']),
-                                 float(vtx.attrib['y']),
-                                 float(vtx.attrib['z'])])
-            mat_p.append(vertices)
-
-    return np.array(mat_c, dtype=np.float32), \
-        np.array(mat_p, dtype=np.float32), \
-        np.array(mat_e, dtype=np.float32), \
-        np.array(mat_spec, dtype=np.float32), \
-        np.array(mat_refl, dtype=np.float32), \
-        np.array(mat_refr, dtype=np.float32)
-
-
-def read_tri(fname):
-    tris = []
-    with open(fname) as f:
-        for line in f:
-            line = line.strip()
-            if line == 'Triangle':
-                colors = f.readline().strip().split()
-                v1 = f.readline().strip().split()
-                v2 = f.readline().strip().split()
-                v3 = f.readline().strip().split()
-                tri = Triangle(v1, v2, v3, colors[:3], colors[3:])
-                tris.append(tri)
-
-    return tris
-
-
-if __name__ == '__main__':
-    import sys
-    tris = read_tri(sys.argv[1])
-
-    print(len(tris))
-
-    for tri in tris:
-        print(tri.fcolor, tri.n)
+        return np.array(mat_c, dtype=np.float32), \
+            np.array(mat_p, dtype=np.float32), \
+            np.array(mat_e, dtype=np.float32), \
+            np.array(mat_spec, dtype=np.float32), \
+            np.array(mat_refl, dtype=np.float32), \
+            np.array(mat_refr, dtype=np.float32)
