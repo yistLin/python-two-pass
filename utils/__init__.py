955ccb2ebef8a2b5d2c57d883d5b2e3675437546
Jy-Liu <junyouliu9@gmail.com>
2018-01-11 12:29:59 +0800

    new format for form factor

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,4 +4,4 @@ from utils.transform_matrix import TransformMatrix
 from utils.reader import XMLReader
 from utils.writer import XMLWriter
 from utils.entity import Entity, Barrel, Cuboid, Globe, Teapot, Tset
-# from utils.formfactor import FormFactor
+from utils.formfactor import FormFactor
